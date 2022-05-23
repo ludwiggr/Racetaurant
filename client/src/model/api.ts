cdf7ce689dea1d2c81c8cc4b1b7f3870bb150dea
Nico Petzendorfer <nico.petzendorfer@tum.de>
2022-05-23 20:43:53 +0200

    Added API that requests data form server

---
+++ client/src/model/api.ts
@@ -0,0 +1,31 @@
+import Restaurant from "./restaurant"
+import PriceCategory from "./priceCategory";
+import Cuisine from "./cuisine";
+import axios from "axios";
+
+const getRestaurants = (filter?: {
+    price?: PriceCategory,
+    latitude?: number,
+    longitude?: number,
+    radius?: number,
+    cuisines?: Array<Cuisine>,
+    rating_min?: number,
+    rating_max?: number,
+    time_start?: Date,
+    time_stop?: Date,
+    persons?: number,
+}, start?: number, limit?: number, order?: "id" | "name" | "rating", asc?: boolean): Promise<Array<Restaurant>> => {
+    return new Promise((resolve, reject) => {
+        axios.get("/api/restaurants", {
+            params: { ...filter, start: start, limit: limit, order: order, asc: asc }
+        }).then(result => resolve(result.data as Array<Restaurant>)).catch(reject);
+    });
+}
+
+const getRestaurantById = (id: string): Promise<Restaurant> => {
+    return new Promise((resolve, reject) => {
+        axios.get(`/api/restaurants/${encodeURIComponent(id)}`).then(result => resolve(result.data as Restaurant)).catch(reject);
+    });
+}
+
+export { getRestaurants, getRestaurantById };
