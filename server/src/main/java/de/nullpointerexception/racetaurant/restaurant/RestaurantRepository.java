d90d6ef72314f8aead6333d4efc25ace4a27cce0
harrydehix <harry.holly02@gmail.com>
2022-05-27 18:39:46 +0200

    Implemented basic restaurant filter query
    Doesn't support filtering by cuisines, time_start, time_stop and persons yet

--- server/src/main/java/de/nullpointerexception/racetaurant/restaurant/RestaurantRepository.java
+++ server/src/main/java/de/nullpointerexception/racetaurant/restaurant/RestaurantRepository.java
@@ -1,9 +1,21 @@
 package de.nullpointerexception.racetaurant.restaurant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, String> {
-	public List<Restaurant> findFiltered();
+
+	@Query("SELECT * FROM restaurants WHERE " +
+			"priceCategory = :price AND " +
+			"rating >= :ratingMin AND " +
+			"rating <= :ratingMax AND " +
+			"POW( ( 69.1 * ( location.longitude - :longitude ) * cos( :latitude / 57.3 ) ) , 2 ) + POW( ( 69.1 * ( location.latitude - :latitude ) ) , 2 )) < ( :radius * :radius ) AND" +
+			"ORDER BY :order :direction " +
+			"LIMIT :limit OFFSET :start"
+	)
+	List<Restaurant> findFiltered(@Param("start") int start, @Param("limit") int limit, @Param("price") int price,  @Param("latitude") double latitude, @Param("longitude") double longitude, @Param("radius") double radius, @Param("ratingMin") double ratingMin, @Param("ratingMax") double ratingMax, @Param("order") String order, @Param("direction") String direction);
+
 }
