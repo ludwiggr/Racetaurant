a79bb4dee8b675e0bea394b184ce01ccef68854d
harrydehix <harry.holly02@gmail.com>
2022-05-28 20:16:47 +0200

    Changed restaurant id datatype to long

--- server/src/main/java/de/nullpointerexception/racetaurant/restaurant/Restaurant.java
+++ server/src/main/java/de/nullpointerexception/racetaurant/restaurant/Restaurant.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity @Table(name = "restaurants") public class Restaurant {
-	@Id @GeneratedValue(strategy = GenerationType.SEQUENCE) private String id;
+	@Id @GeneratedValue(strategy = GenerationType.SEQUENCE) private Long id;
 	private String name;
 	private String website;
 	private double rating;
@@ -35,7 +35,7 @@ import java.util.List;
 		this.times = times;
 	}
 
-	public String getId() {
+	public Long getId() {
 		return id;
 	}
 
