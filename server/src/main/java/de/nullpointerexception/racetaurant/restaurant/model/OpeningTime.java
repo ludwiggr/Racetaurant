5a8bc44b38a71bad24d4730268324c2cda5842eb
harrydehix <harry.holly02@gmail.com>
2022-05-29 14:57:50 +0200

    Fixed typo

--- server/src/main/java/de/nullpointerexception/racetaurant/restaurant/model/OpeningTime.java
+++ server/src/main/java/de/nullpointerexception/racetaurant/restaurant/model/OpeningTime.java
@@ -7,9 +7,9 @@ import javax.validation.constraints.Pattern;
 @Embeddable public class OpeningTime {
 	private static final String CLOSED = "closed";
 
-	@NotNull @Pattern(regexp = "^(([01]\\d|2[0-3]):([0-5]\\d)|closed)$", message = "Invalid time format for 'mondayFrom'.") private String from = CLOSED;
+	@NotNull @Pattern(regexp = "^(([01]\\d|2[0-3]):([0-5]\\d)|closed)$", message = "Invalid time format for 'from'.") private String from = CLOSED;
 
-	@NotNull @Pattern(regexp = "^(([01]\\d|2[0-3]):([0-5]\\d)|closed)$", message = "Invalid time format for 'mondayTo'.") private String to = CLOSED;
+	@NotNull @Pattern(regexp = "^(([01]\\d|2[0-3]):([0-5]\\d)|closed)$", message = "Invalid time format for 'to'.") private String to = CLOSED;
 
 	public OpeningTime() {
 		this.from = CLOSED;
